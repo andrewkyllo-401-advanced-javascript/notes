568226ddaac7aa6e2b19145719f2745f5a1eee70
Andrew Kyllo <kyllo34@gmail.com>
2020-02-12 17:06:26 -0800

    added gitignore

--- index.js
+++ index.js
@@ -2,11 +2,21 @@
 
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
+const mongoose = require('mongoose');
+
+
+const MONGOOSE_URI = 'mongodb://localhost:27017/notes';
+mongoose.connect(MONGOOSE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const input = new Input();
-const notes = new Notes(input);
+const notes = new Notes();
 
-input.valid() ? notes.execute(input) : help();
+input.valid() ? notes.execute(input).then(data => {mongoose.disconnect()}).catch(error => {console.error(error)}) 
+  : help();
+
 
 
 
