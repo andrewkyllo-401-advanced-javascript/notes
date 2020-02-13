21d9557294238fb8d2508345b05eb5b9c0b6aac7
Andrew Kyllo <kyllo34@gmail.com>
2020-02-12 16:54:50 -0800

    Revert "created async functions now stores to databse"
    This reverts commit c06e1edf7edb1c2198571f84f09ec601e2dce7bf.

--- index.js
+++ index.js
@@ -2,21 +2,11 @@
 
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
-const mongoose = require('mongoose');
-
-
-const MONGOOSE_URI = 'mongodb://localhost:27017/notes';
-mongoose.connect(MONGOOSE_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 
 const input = new Input();
-const notes = new Notes();
+const notes = new Notes(input);
 
-input.valid() ? notes.execute(input).then(data => {mongoose.disconnect()}).catch(error => {console.error(error)}) 
-  : help();
-
+input.valid() ? notes.execute(input) : help();
 
 
 
--- models/notes-schema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const notesSchema = mongoose.Schema({
-  category: { type: String, required: true},
-  text: { type: String, required: true}
-});
-
-module.exports = mongoose.model('notes', notesSchema);
