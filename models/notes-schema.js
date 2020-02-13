c06e1edf7edb1c2198571f84f09ec601e2dce7bf
Andrew Kyllo <kyllo34@gmail.com>
2020-02-12 16:30:57 -0800

    created async functions now stores to databse

---
+++ models/notes-schema.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const notesSchema = mongoose.Schema({
+  category: { type: String, required: true},
+  text: { type: String, required: true}
+});
+
+module.exports = mongoose.model('notes', notesSchema);
